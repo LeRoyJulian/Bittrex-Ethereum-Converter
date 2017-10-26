61ad27b8ae40f3285ef06786e270cc2bfd2ebbc8
Julian Vogin <julian@bartounet.fr>
2017-10-26 03:41:35 +0200

    Updates

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Convert prices in ETH on Bittrex.com
 To install it, go to Chrome Extensions, check Developer Mode On, drag and drop the folder there, then verify it’s activated.
 Check the source code before installing if you don't trust me, it's only simple javascript!
+
+![Screenshot](https://i.imgur.com/egLBp5P.png)
--- content.js
+++ content.js
@@ -5,7 +5,7 @@ function convertPrice() {
   $.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=ETH').done(function(data) {
     var orig = parseFloat($('.base-market > span').first().text())
     var price = parseFloat(data[0].price_eth)
-    $('h2').text(Number(orig / price).toFixed(8) + ' ETH')
+    $('.market-stats h2').text(Number(orig / price).toFixed(8) + ' ETH')
     console.log($('.wrapper > .base-market > span').first().text())
   })
   setTimeout(function() { convertPrice() }, 2500)
